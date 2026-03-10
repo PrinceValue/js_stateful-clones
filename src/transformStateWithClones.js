afe3e867fcad7cb6a40e864feb39e398d42127c4
PrinceValue <princeossoa@gmail.com>
2026-03-10 22:26:21 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = Object.assign({}, state);
+  const stateChangesLog = [];
+
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      for (const key of Object.keys(i.extraData)) {
+        stateClone[key] = i.extraData[key];
+      }
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const n of i.keysToRemove) {
+        for (const key of Object.keys(stateClone)) {
+          if (key === n) {
+            delete stateClone[key];
+          }
+        }
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const key of Object.keys(stateClone)) {
+        delete stateClone[key];
+      }
+    }
+
+    stateChangesLog.push({ ...stateClone });
+  }
+
+  return stateChangesLog;
 }
 
 module.exports = transformStateWithClones;
